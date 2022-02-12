a8378886bc158d7e1a54697bd40a6e165e68955d
Waliul Islam <waislam67@gmail.com>
2022-02-12 15:08:16 +0600

    vsf2 added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, send_file
 import os
+from vsf2.vsf3 import Appointment
 
 app.config['UPLOAD_FOLDER'] = 'app/static/csv'
 
@@ -12,6 +13,11 @@ def index():
         if csvfile.filename != '':
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], csvfile.filename)
             csvfile.save(filepath)
-
-
     return render_template("index.html")
+
+@app.route('/run', methods = ['GET'])
+def run():
+    bot = Appointment()
+    bot.run()
+    return "bot is running"
+
