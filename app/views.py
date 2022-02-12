02fd07f6e7b4ab142b000f3cf7243f5c3507cb38
Waliul Islam <waislam67@gmail.com>
2022-02-12 12:30:01 +0600

    first one

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from app import app
+from flask import render_template, request
+import os
+
+app.config['UPLOAD_FOLDER'] = 'static/csv'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        csvfile = request.files['csvfile']
+        print(csvfile)
+        if csvfile.filename != '':
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], csvfile.filename)
+            csvfile.save(filepath)
+
+
+    return render_template("index.html")
